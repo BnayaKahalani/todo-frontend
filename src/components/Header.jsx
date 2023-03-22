2d8ffbf20b64a7128d1b2e7a003bea324d07abcd
Bnaya Kahalani <bnaya50@gmail.com>
2023-03-22 10:50:03 +0200

    add hide-on-scroll functionality to app bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
-import { React, useState } from "react"
+import * as React from "react"
+import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
 
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, MenuItem, Dialog } from "@mui/material"
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  MenuItem,
+  Dialog,
+  CssBaseline,
+  useScrollTrigger,
+  Slide,
+} from "@mui/material"
+
 import { SportsSoccer as SportsSoccerIcon } from "@mui/icons-material"
 
 import { useLogout } from "../hooks/useLogout"
@@ -41,26 +57,91 @@ const Header = () => {
     setOpen(false)
   }
 
+  const HideOnScroll = (props) => {
+    const { children, window } = props
+
+    const trigger = useScrollTrigger({
+      target: window ? window() : undefined,
+    })
+
+    return (
+      <Slide
+        appear={false}
+        direction='down'
+        in={!trigger}
+      >
+        {children}
+      </Slide>
+    )
+  }
+
   return (
-    <AppBar
-      style={{ background: "white", color: "black" }}
-      position='static'
-    >
-      <Container maxWidth='xl'>
-        <Toolbar
-          disableGutters
-          sx={{ justifyContent: "space-between" }}
-        >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <SportsSoccerIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+    <HideOnScroll>
+      <AppBar
+        style={{ background: "white", color: "black" }}
+        position='sticky'
+      >
+        <Container maxWidth='xl'>
+          <Toolbar
+            disableGutters
+            sx={{ justifyContent: "space-between" }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <SportsSoccerIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant='h6'
+                noWrap
+                component='a'
+                href='/'
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                GoalGuru
+              </Typography>
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              {user && (
+                <div>
+                  <span
+                    className='material-symbols-outlined add'
+                    onClick={handleClickOpen}
+                  >
+                    add
+                  </span>
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <TodoForm onClose={handleClose} />
+                  </Dialog>
+                </div>
+              )}
+              <IconButton
+                size='large'
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                onClick={handleOpenNavMenu}
+                color='inherit'
+              ></IconButton>
+            </Box>
+            <SportsSoccerIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
             <Typography
-              variant='h6'
+              variant='h5'
               noWrap
               component='a'
-              href='/'
+              href=''
               sx={{
                 mr: 2,
-                display: { xs: "none", md: "flex" },
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
                 fontFamily: "monospace",
                 fontWeight: 700,
                 color: "inherit",
@@ -69,105 +150,60 @@ const Header = () => {
             >
               GoalGuru
             </Typography>
-          </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             {user && (
-              <div>
-                <span
-                  className='material-symbols-outlined add'
-                  onClick={handleClickOpen}
+              <Box sx={{ flexGrow: 0 }}>
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ p: 0 }}
                 >
-                  add
-                </span>
-                <Dialog
-                  open={open}
-                  onClose={handleClose}
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='/static/images/avatar/2.jpg'
+                  />
+                </IconButton>
+
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id='menu-appbar'
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={() => setAnchorElUser(null)}
                 >
-                  <TodoForm onClose={handleClose} />
-                </Dialog>
+                  <MenuItem
+                    key='userSettings'
+                    onClick={() => handleUserMenuClick("/user-settings")}
+                  >
+                    <Typography textAlign='center'>Settings</Typography>
+                  </MenuItem>
+                  <MenuItem
+                    key='logout'
+                    onClick={handleLogout}
+                  >
+                    <Typography textAlign='center'>Logout</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            )}
+            {!user && (
+              <div className='login-logout'>
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Signup</Link>
               </div>
             )}
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='inherit'
-            ></IconButton>
-          </Box>
-          <SportsSoccerIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant='h5'
-            noWrap
-            component='a'
-            href=''
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            GoalGuru
-          </Typography>
-
-          {user && (
-            <Box sx={{ flexGrow: 0 }}>
-              <IconButton
-                onClick={handleOpenUserMenu}
-                sx={{ p: 0 }}
-              >
-                <Avatar
-                  alt='Remy Sharp'
-                  src='/static/images/avatar/2.jpg'
-                />
-              </IconButton>
-
-              <Menu
-                sx={{ mt: "45px" }}
-                id='menu-appbar'
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={() => setAnchorElUser(null)}
-              >
-                <MenuItem
-                  key='userSettings'
-                  onClick={() => handleUserMenuClick("/user-settings")}
-                >
-                  <Typography textAlign='center'>Settings</Typography>
-                </MenuItem>
-                <MenuItem
-                  key='logout'
-                  onClick={handleLogout}
-                >
-                  <Typography textAlign='center'>Logout</Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
-          )}
-          {!user && (
-            <div className='login-logout'>
-              <Link to='/login'>Login</Link>
-              <Link to='/signup'>Signup</Link>
-            </div>
-          )}
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
   )
 }
 
